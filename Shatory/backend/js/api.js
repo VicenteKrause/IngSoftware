a1dcf6200301872fbc136f6699ac6c853aae0182
Lucas Cruz <lcruz@chilexpress.cl>
2022-09-21 20:40:14 -0300

    Creacion Backend

---
+++ Shatory/backend/js/api.js
@@ -0,0 +1,26 @@
+"use strict";
+const express = require('express');
+const app = express();
+const port = 3001;
+const bodyParser = require('body-parser');
+const cors = require('cors');
+// var connection = mysql.createConnection({
+//     host    : process.env.HOST,
+//     user    : process.env.USER,
+//     port    : process.env.BD_PORT,
+//     password: process.env.BD_PASSWORD,
+//     database: process.env.DATANAME
+// });
+// connection.connect(function(error:any){
+//     if(error){
+//         console.log('No conectado')
+//         return;
+//     }
+//     console.log('Conectado')
+// });
+app.get('/', (req, res) => {
+    res.send('Hello World');
+});
+app.listen(port, () => {
+    console.log(`Escuchando en puerto ${port}`);
+});
